e1bd9c2cc59fceaad7de5cdff863e2d6345155a3
ramypractice <146600666+ramypractice@users.noreply.github.com>
2023-10-17 18:38:03 +0630

    1

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-alert {
-    heloo
-}
+function sayHello(name) {
+    if (name.toLowerCase() === 'john') {
+      return 'Hello Ali';
+    } else {
+      return 'Hello ' + name;
+    }
+  }
+  
+  // Example usage:
+  console.log(sayHello('John')); // Output: Hello Ali
+  console.log(sayHello('Alice')); // Output: Hello Alice
+  
