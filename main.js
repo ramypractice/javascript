3d819e703b5845bd3676d4f420ac2677b413330e
ramypractice <146600666+ramypractice@users.noreply.github.com>
2023-10-17 18:49:02 +0630

    q

--- main.js
+++ main.js
@@ -9,4 +9,14 @@ function sayHello(name) {
   // Example usage:
   console.log(sayHello('John')); // Output: Hello Ali
   console.log(sayHello('Ali')); // Output: Hello Alice
+
+
+  // Wait for the document to be fully loaded
+$(document).ready(function() {
+    // Code inside this block will run when the document is ready
+
+    // Hide the element with ID "myElement"
+    $('#myElement').hide();
+});
+
   
