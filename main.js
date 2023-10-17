880aff1099d96e800c7d27e45c9a46aed2a10a58
ramypractice <146600666+ramypractice@users.noreply.github.com>
2023-10-17 19:21:57 +0630

    upld

--- main.js
+++ main.js
@@ -1,22 +1,51 @@
-function sayHello(name) {
-    if (name.toLowerCase() === 'john') {
-      return 'Hello Ali';
-    } else {
-      return 'Hello ' + name;
-    }
-  }
-  
-  // Example usage:
-  console.log(sayHello('John')); // Output: Hello Ali
-  console.log(sayHello('Ali')); // Output: Hello Alice
+        // Function to create a message from a person
+        function createMessage(person, message) {
+            return person + ': ' + message;
+        }
 
+        // Variables representing two people
+        var person1 = 'John';
+        var person2 = 'Ali';
 
-  // Wait for the document to be fully loaded
-$(document).ready(function() {
-    // Code inside this block will run when the document is ready
+        // Conversation between the two people
+        var message1 = createMessage(person1, 'Hello, how are you?');
+        var message2 = createMessage(person2, 'Hi John, I am doing well. How about you?');
 
-    // Hide the element with ID "myElement"
-    $('#myElement').hide();
-});
+        // Printing the conversation
+        console.log(message1);
+        console.log(message2);
 
-  
+                
+        
+        // Function to create a message from a person
+        function createMessage(person, message) {
+            return person + ': ' + message;
+        }
+
+        // Variables representing two people
+        var person1 = 'John';
+        var person2 = 'Ali';
+
+        // Conversation between the two people
+        var message1 = createMessage(person1, 'Hello, how are you?');
+        var message2 = createMessage(person2, 'Hi John, I am doing well. How about you?');
+
+        // Displaying the conversation in the specified div with typing animation
+        var conversationDiv = document.getElementById('conversation');
+
+        function displayMessage(message, speed) {
+            var index = 0;
+            var typingEffect = setInterval(function() {
+                conversationDiv.innerHTML = message.slice(0, index);
+                index++;
+                if (index > message.length) {
+                    clearInterval(typingEffect);
+                }
+            }, speed);
+        }
+
+        // Display the messages with typing animation
+        displayMessage(message1, 100);
+        setTimeout(function() {
+            displayMessage(message2, 100);
+        }, message1.length * 100 + 1000); // Wait for message1 to finish and then display message2
